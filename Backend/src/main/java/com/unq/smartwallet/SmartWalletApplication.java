1c3792e83624eaf6749a20eebf3169b763163231
Lucas Maroso <marosolucas@gmail.com>
2021-04-19 04:12:00 -0300

    Kickoff!

---
+++ Backend/src/main/java/com/unq/smartwallet/SmartWalletApplication.java
@@ -0,0 +1,13 @@
+package com.unq.smartwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartWalletApplication.class, args);
+	}
+
+}
