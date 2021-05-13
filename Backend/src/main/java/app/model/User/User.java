f2733f92101b1ecebd7fe44bc3ee514b3606bb06
Fede <fedejmartinez@gmail.com>
2021-05-12 21:52:40 -0300

    Agrega modelo de usuario

---
+++ Backend/src/main/java/app/model/User/User.java
@@ -0,0 +1,111 @@
+package app.model.User;
+
+import app.model.Exceptions.InvalidEmailException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.Collection;
+import java.util.Collections;
+import static app.model.User.UserRole.USER;
+
+@Entity
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "type", discriminatorType = DiscriminatorType.STRING)
+public class User implements UserDetails {
+
+    //Parameters
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotEmpty
+    private String name;
+
+    @NotEmpty
+    private String email;
+
+    @NotEmpty
+    private String password;
+
+    private double accountCredit;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    private Boolean expired;
+
+    private Boolean locked;
+
+    private Boolean enabled;
+
+    //Constructor
+    public User() {}
+
+    public User(String name, String email, String password) {
+
+        isAValidEmail(email);
+
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.accountCredit = 0;
+        this.role = USER;
+        this.expired = false;
+        this.locked = false;
+        this.enabled = false;
+    }
+
+
+    //Methods
+    public String getName(){
+        return this.name;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    private void isAValidEmail(String newEmail) {
+        if (!newEmail.matches("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"))
+            throw new InvalidEmailException(newEmail);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(this.role.name());
+        return Collections.singletonList(authority);
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return !this.expired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !this.locked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+}
