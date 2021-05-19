92f73d61f3832a02f58e9b33b0b49f2651f39a30
Amalia Jaime <amaliajaime3@gmail.com>
2021-05-19 17:38:03 -0300

    Se agrega el modelo y el endpoint para crear un Gasto

--- Backend/src/main/java/app/dto/ExpenseDTO.java
+++ Backend/src/main/java/app/dto/ExpenseDTO.java
@@ -1,2 +1,70 @@
-package app.dto;public class ExpenseDTO {
+package app.dto;
+
+import java.time.LocalDateTime;
+
+public class ExpenseDTO {
+
+    public String type;
+    public String name;
+    public String description;
+    public Integer amount;
+    public LocalDateTime date;
+    public Boolean programmed;
+
+    public ExpenseDTO(String type, String name, String description, Integer amount, LocalDateTime date, Boolean programmed){
+        this.type = type;
+        this.name = name;
+        this.description = description;
+        this.amount = amount;
+        this.date = date;
+        this.programmed = programmed;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public Boolean getProgrammed() {
+        return programmed;
+    }
+
+    public void setProgrammed(Boolean programmed) {
+        this.programmed = programmed;
+    }
 }
