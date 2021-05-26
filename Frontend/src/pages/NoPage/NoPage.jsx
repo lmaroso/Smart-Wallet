8f1e4887168e36ab2b52d64292d9b4b3d5c34df9
Lucas Maroso <marosolucas@gmail.com>
2021-05-25 22:51:18 -0300

    Agrego vista para la pagina por defecto

---
+++ Frontend/src/pages/NoPage/NoPage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { IonPage, IonImg } from "@ionic/react";
+
+import Button from "../../components/Button/Button";
+
+import "./NoPage.scss";
+
+const NoPage = ({ history }) => (
+	<IonPage className="content">
+		<div className="imageContainer">
+			<IonImg src="/images/not-found.png"/>
+			<Button expand="block" onClick={() => history.push({ pathname: "/" })}>
+                Ir al inicio
+			</Button>
+		</div>
+	</IonPage>
+);
+
+export default NoPage;
--- Frontend/src/routes.jsx
+++ Frontend/src/routes.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
+import NoPage from "./pages/NoPage/NoPage";
 import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Income from "./pages/Income/Income";
+import Profile from "./pages/Profile/Profile";
+import Expense from "./pages/Expense/Expense";
 
 const Routes = () => (
 	<BrowserRouter>
@@ -12,12 +15,14 @@ const Routes = () => (
 			history.listen((location) => window._mfq && window._mfq.push(["newPageView", location.pathname]));
 			return (
 				<Switch location={location}>
-					{/* <Route exact component={NoPage} path="/404" /> */}
 					<Route exact component={Register} path="/" />
 					<Route exact component={Register} path="/register" />
 					<Route exact component={Login} path="/login" />
 					<Route exact component={Dashboard} path="/dashboard" />
 					<Route exact component={Income} path="/income" />
+					<Route exact component={Profile} path="/profile" />
+					<Route exact component={Expense} path="/expense" />
+					<Route exact component={NoPage} path="*" />
 				</Switch>
 			);}
 		} />
