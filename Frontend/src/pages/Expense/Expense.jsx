46e460fabb10e73dccda6b5d60292331457d1154
Lucas Maroso <marosolucas@gmail.com>
2021-05-25 22:53:45 -0300

    Agrego vista de nuevo gasto

---
+++ Frontend/src/pages/Expense/Expense.jsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+
+import ExpenseView from "./ExpenseView";
+
+import { addIncome } from "../../services/api";
+import { getKey } from "../../utils/localStorage";
+
+const Expense = ({ history }) => {
+
+	const [name, setName] = useState("");
+	const [description, setDescription] = useState("");
+	const [amount, setAmount] = useState(0);
+	const [programmed, setProgrammed] = useState(null);
+	const [shouldShowToast, setShouldShowToast] = useState(false);
+	const [toastType, setToastType] = useState("success");
+	const [toastText, setToastText] = useState("");
+
+	useEffect(() => {
+		if(!getKey("token")) {
+			history.push({ pathname: "/" });
+		}
+	}, [history]);
+
+	const onSubmit = (event) => {
+		event.preventDefault();
+		const date = moment().format("YYYY-MM-DD[T]HH:mm:ss");
+		addIncome({ name, description, amount, date, programmed })
+			.then(({ status, data }) => {
+				if (status === 200 || status === 201) {
+					setToastType("success");
+					setToastText("Guardado exitosamente");
+					setTimeout(() => {
+						history.push({ pathname: "/dashboard" });
+					}, 4000);
+				} else if (status >= 400) {
+					setToastType("error");
+					setToastText(data);
+				}
+				setShouldShowToast(true);
+			});
+	};
+	return (
+		<ExpenseView
+			amount={amount}
+			description={description}
+			name={name}
+			programmed={programmed}
+			setAmount={setAmount}
+			setDescription={setDescription}
+			setName={setName}
+			setProgrammed={setProgrammed}
+			setShouldShowToast={setShouldShowToast}
+			shouldShowToast={shouldShowToast}
+			toastText={toastText}
+			toastType={toastType}
+			onSubmit={onSubmit}
+		/>
+	);
+};
+
+export default Expense;
---
+++ Frontend/src/pages/Expense/ExpenseView.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+
+import PageWrapper from "../../components/PageWrapper/PageWrapper";
+import Button from "../../components/Button/Button";
+import Input from "../../components/Input/Input";
+import Selector from "../../components/Selector/Selector";
+import SelectorItem from "../../components/Selector/SelectorItem";
+import Toast from "../../components/Toast/Toast";
+
+const selectorOptions = () => {
+	const options = [
+		{
+			name: "Sí",
+			value: true
+		}, {
+			name: "No",
+			value: false
+		}
+	];
+	return options.map(
+		option => <SelectorItem key={option.name} name={option.name} value={option.value} />
+	);
+};
+
+const ExpenseView = ({
+	amount,
+	description,
+	name,
+	programmed,
+	setAmount,
+	setDescription,
+	setName,
+	setProgrammed,
+	setShouldShowToast,
+	shouldShowToast,
+	toastText,
+	toastType,
+	onSubmit
+}) => (
+	<PageWrapper>
+		<form onSubmit={onSubmit}>
+			<Input
+				inputmode="text"
+				label="Nombre"
+				placeholder="Ingrese un nombre del nuevo gasto"
+				required={true}
+				type="text"
+				value={name}
+				onChange={event => setName(event.detail.value)}
+			/>
+			<Input
+				inputmode="text"
+				label="Descripción"
+				placeholder="Descripción del nuevo gasto"
+				required={true}
+				type="text"
+				value={description}
+				onChange={event => setDescription(event.detail.value)}
+			/>
+			<Input
+				inputmode="numeric"
+				label="Monto"
+				placeholder="Ingrese un monto"
+				required={true}
+				type="number"
+				value={amount}
+				onChange={event => setAmount(parseInt(event.detail.value))}
+			/>
+			<Selector
+				placeholder="¿Es un gasto programado?"
+				value={programmed}
+				onChange={event => setProgrammed(event.detail.value)}
+			>
+				{selectorOptions()}
+			</Selector>
+			<Button expand="block" type="submit">
+                Guardar
+			</Button>
+		</form>
+		<Toast isOpen={shouldShowToast} message={toastText} type={toastType} onDidDismiss={() => setShouldShowToast(false)} />
+	</PageWrapper>
+);
+
+export default ExpenseView;
