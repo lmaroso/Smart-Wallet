60843fb0419bf32f81a31fcae11f961d72c59453
Amalia Jaime <amaliajaime3@gmail.com>
2021-06-01 02:23:56 -0300

    HISTORY EXPENSES - Se modifica Exeption

--- Backend/src/main/java/app/model/Exceptions/NotFoundExpense.java
+++ Backend/src/main/java/app/model/Exceptions/NotFoundExpense.java
@@ -3,7 +3,7 @@ package app.model.Exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+@ResponseStatus(code = HttpStatus.OK)
 public class NotFoundExpense extends RuntimeException{
 
     private final static String NOT_FOUND_EXPENSE = "Not found expense";
