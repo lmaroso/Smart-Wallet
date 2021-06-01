979fcebf568520c1ed9854fe49e977bc800bc634
Amalia Jaime <amaliajaime3@gmail.com>
2021-06-01 19:07:34 -0300

    USER - Se agregan unit test

--- Backend/src/test/java/unitTests/UserTests.java
+++ Backend/src/test/java/unitTests/UserTests.java
@@ -1,8 +1,11 @@
 package unitTests;
 
 import app.model.Exceptions.InvalidEmailException;
+import app.model.Expense.Expense;
 import app.model.User.User;
 import org.junit.Test;
+
+import java.time.LocalDateTime;
 
 import static org.junit.Assert.*;
 
@@ -22,6 +25,42 @@ public class UserTests {
         assertTrue(user.isAccountNonLocked());
         assertTrue(user.isCredentialsNonExpired());
         assertFalse(user.isEnabled());
+
+    }
+
+    @Test
+    public void setName(){
+
+        User user = new User("Smart", "smart.wallet.app1@gmail.com", "sw");
+
+        String expect = "Sm";
+        user.setName("Sm");
+
+        assertEquals(expect, user.getName());
+
+    }
+
+    @Test
+    public void setEmail(){
+
+        User user = new User("Smart", "smart.wallet.app1@gmail.com", "sw");
+
+        String expect = "smart.wallet.app@com";
+        user.setEmail("smart.wallet.app@com");
+
+        assertEquals(expect, user.getUsername());
+
+    }
+
+    @Test
+    public void setPassword(){
+
+        User user = new User("Smart", "smart.wallet.app1@gmail.com", "sw");
+
+        String expect = "SmartW";
+        user.setPassword("SmartW");
+
+        assertEquals(expect, user.getPassword());
 
     }
 
