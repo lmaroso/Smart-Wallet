7b5c2c0a340765bc073896f6f82c3ab6ffc1e4c5
Amalia Jaime <amaliajaime3@gmail.com>
2021-06-01 18:44:49 -0300

    INCOME / EXPENSE - Se agregan unit test

--- Backend/src/test/java/unitTests/ExpenseTests.java
+++ Backend/src/test/java/unitTests/ExpenseTests.java
@@ -26,6 +26,65 @@ public class ExpenseTests {
     }
 
     @Test
+    public void setUserId(){
+
+        Expense expense = new Expense(Long.valueOf(1), "Alquiler", "Alquiler mensual", 35000, LocalDateTime.now(), false);
+
+        Long expect = Long.valueOf(2);
+        expense.setUserId(Long.valueOf(2));
+
+        assertEquals(expect, expense.getUserId());
+
+    }
+
+    @Test
+    public void setName(){
+
+        Expense expense = new Expense(Long.valueOf(1), "Alquileres", "Alquiler mensual", 35000, LocalDateTime.now(), false);
+
+        String expect = "Alquiler";
+        expense.setName("Alquiler");
+
+        assertEquals(expect, expense.getName());
+
+    }
+
+    @Test
+    public void setDescription(){
+
+        Expense expense = new Expense(Long.valueOf(1), "Alquiler", "Alquiler mensual", 35000, LocalDateTime.now(), false);
+
+        expense.setDescription("Mensual");
+        String expect = "Mensual";
+
+        assertEquals(expect, expense.getDescription());
+
+    }
+
+    @Test
+    public void setAmount(){
+
+        Expense expense = new Expense(Long.valueOf(1), "Alquiler", "Alquiler mensual", 35000, LocalDateTime.now(), false);
+
+        Integer expect = 30000;
+        expense.setAmount(30000);
+
+        assertEquals(expect, expense.getAmount());
+
+    }
+
+    @Test
+    public void setDate(){
+        Expense expense = new Expense(Long.valueOf(1), "Alquiler", "Alquiler mensual", 35000, LocalDateTime.now(), false);
+
+        LocalDateTime expect = LocalDateTime.of(2021, 05, 31, 17, 20, 00);
+        expense.setDate(LocalDateTime.of(2021, 05, 31, 17, 20, 00));
+
+        assertEquals(expect,expense.getDate() );
+
+    }
+
+    @Test
     public void setProgrammedExpense(){
 
         Expense expense = new Expense(Long.valueOf(1), "Alquiler", "Alquiler mensual", 35000, LocalDateTime.now(), false);
@@ -33,6 +92,17 @@ public class ExpenseTests {
         expense.setProgrammed(true);
 
         assertTrue(expense.isProgrammed());
+
+    }
+
+    @Test
+    public void setAvailableExpense(){
+
+        Expense expense = new Expense(Long.valueOf(1), "Alquiler", "Alquiler mensual", 0, LocalDateTime.now(), false);
+
+        expense.setAmount(20000);
+
+        assertTrue(expense.isAvailable());
 
     }
 
--- Backend/src/test/java/unitTests/IncomeTests.java
+++ Backend/src/test/java/unitTests/IncomeTests.java
@@ -1,5 +1,6 @@
 package unitTests;
 
+import app.model.Expense.Expense;
 import app.model.Income.Income;
 import org.junit.Test;
 
@@ -26,13 +27,84 @@ public class IncomeTests {
     }
 
     @Test
-    public void setProgrammedExpense(){
+    public void setUserId(){
+
+        Income income = new Income(Long.valueOf(1), "Sueldo", "Sueldo mensual", 35000, LocalDateTime.now(), false);
+
+        Long expect = Long.valueOf(2);
+        income.setUserId(Long.valueOf(2));
+
+        assertEquals(expect, income.getUserId());
+
+    }
+
+    @Test
+    public void setName(){
+
+        Income income = new Income(Long.valueOf(1), "Sueldo", "Sueldo mensual", 35000, LocalDateTime.now(), false);
+
+        String expect = "Sueldo bruto";
+        income.setName("Sueldo bruto");
+
+        assertEquals(expect, income.getName());
+
+    }
+
+    @Test
+    public void setDescription(){
+
+        Income income = new Income(Long.valueOf(1), "Sueldo", "Sueldo mensual", 35000, LocalDateTime.now(), false);
+
+        income.setDescription("Mensual");
+        String expect = "Mensual";
+
+        assertEquals(expect, income.getDescription());
+
+    }
+
+    @Test
+    public void setAmount(){
+
+        Income income = new Income(Long.valueOf(1), "Sueldo", "Sueldo mensual", 35000, LocalDateTime.now(), false);
+
+        Integer expect = 40000;
+        income.setAmount(40000);
+
+        assertEquals(expect, income.getAmount());
+
+    }
+
+    @Test
+    public void setDate(){
+        Income income = new Income(Long.valueOf(1), "Sueldo", "Sueldo mensual", 35000, LocalDateTime.now(), false);
+
+        LocalDateTime expect = LocalDateTime.of(2021, 05, 31, 17, 20, 00);
+        income.setDate(LocalDateTime.of(2021, 05, 31, 17, 20, 00));
+
+        assertEquals(expect,income.getDate() );
+
+    }
+
+
+    @Test
+    public void setProgrammedIncome(){
 
         Income income = new Income(Long.valueOf(1), "Sueldo", "Sueldo mensual", 35000, LocalDateTime.now(), false);
 
         income.setProgrammed(true);
 
         assertTrue(income.isProgrammed());
+
+    }
+
+    @Test
+    public void setAvailableIncome(){
+
+        Income income = new Income(Long.valueOf(1), "Sueldo", "Sueldo mensual", 0, LocalDateTime.now(), false);
+
+        income.setAmount(20000);
+
+        assertTrue(income.isAvailable());
 
     }
 
