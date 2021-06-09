a7d8b5dccd7ecd19775803fcfa5d89c918fe2b6f
Amalia Jaime <amaliajaime3@gmail.com>
2021-06-08 23:55:55 -0300

    CORRECCIÓN ERROR EDIT PROFILE - Se corrige error al editar perfil

--- Backend/src/main/java/app/api/income/IncomeRepository.java
+++ Backend/src/main/java/app/api/income/IncomeRepository.java
@@ -1,7 +1,6 @@
 package app.api.income;
 
 import app.model.Income.Income;
-import jdk.nashorn.internal.objects.annotations.Getter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
