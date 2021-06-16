fc90bf61196119f662cc38557a96231a3686eca6
Lucas Maroso <marosolucas@gmail.com>
2021-06-16 18:52:05 -0300

    Agrego balance actual

--- Frontend/src/pages/Dashboard/Dashboard.jsx
+++ Frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,22 +1,50 @@
-import React  from "react";
+import React, { useState } from "react";
 import { useIonViewDidEnter } from "@ionic/react";
 
-import PageWrapper from "../../components/PageWrapper/PageWrapper";
+import DashboardView from "./DashboardView";
 
 import { getKey } from "../../utils/localStorage";
+import { getBalance } from "../../services/api";
 
 const Dashboard = ({ history }) => {
 
+	const [loading, setLoading] = useState(false);
+	const [toastType, setToastType] = useState("success");
+	const [toastText, setToastText] = useState("");
+	const [shouldShowToast, setShouldShowToast] = useState(false);
+	const [balance, setBalance] = useState(null);
+
 	useIonViewDidEnter(() => {
-		if(!getKey("token")) {
+		setLoading(true);
+		if(getKey("token")) {
+			getBalance()
+				.then(({ status, data }) => {
+					setTimeout(() => {
+						if (status === 200 || status === 201) {
+							setBalance(data);
+							setLoading(false);
+						} else if (status !== 400) {
+							setToastType("error");
+							setToastText(data);
+							setLoading(false);
+							setShouldShowToast(true);
+						} else setLoading(false);
+					}, 1000);
+				});
+		} else {
 			history.push({ pathname: "/login" });
 		}
 	}, [history]);
 
 	return (
-		<PageWrapper>
-			Dashboard
-		</PageWrapper>
+		<DashboardView
+			balance={balance}
+			loading={loading}
+			setShouldShowToast={setShouldShowToast}
+			shouldShowToast={shouldShowToast}
+			toastText={toastText}
+			toastType={toastType}
+		/>
 	);
 };
 
